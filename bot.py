0869172d0a97392f91b903d10a1a069edca5da15
abraxas0001 <abraxas0001@github.com>
2025-12-07 14:34:15 +0530

    Branding: add  to download captions

--- bot.py
+++ bot.py
@@ -1525,7 +1525,7 @@ def handle_yt_upload_callback(call):
         fname = f"video_{best.get('resolution', 'best')}.mp4"
         size_bytes = best.get('size_bytes')
         base_caption = f"<b>✅ YouTube Video</b>\n<b>Quality:</b> {best.get('resolution') or best.get('extension')}"
-        caption = base_caption + "\n\n<i>Downloaded by @TeraInstaShortsDownloaderbot</i>"
+        caption = base_caption + "\n\n<i>Downloaded by 💜 @TeraInstaShortsDownloaderbot</i>"
 
         # If selected format is video_only, attempt mux with best available audio
         if best.get('type') == 'video_only':
@@ -1558,7 +1558,7 @@ def handle_yt_upload_callback(call):
                                 except Exception:
                                     pass
                                 if out_path.exists():
-                                    caption = base_caption + f"\n<b>Audio merged:</b> {audio_best.get('extension').upper()}" + "\n\n<i>Downloaded by @TeraInstaShortsDownloaderbot</i>"
+                                    caption = base_caption + f"\n<b>Audio merged:</b> {audio_best.get('extension').upper()}" + "\n\n<i>Downloaded by 💜 @TeraInstaShortsDownloaderbot</i>"
                                     with open(out_path, 'rb') as f:
                                         bot.send_video(chat_id, f, caption=caption, supports_streaming=True)
                                     bot.delete_message(chat_id, call.message.message_id)
@@ -1686,7 +1686,7 @@ def handle_yt_audio_callback(call):
                         audio_files = list(Path(tmpdir).glob('*.*'))
                         if audio_files:
                             audio_path = audio_files[0]
-                            caption = f"<b>🎵 YouTube Audio</b>\n<b>Title:</b> {title}\n\n<i>Downloaded by @TeraInstaShortsDownloaderbot</i>"
+                            caption = f"<b>🎵 YouTube Audio</b>\n<b>Title:</b> {title}\n\n<i>Downloaded by 💜 @TeraInstaShortsDownloaderbot</i>"
                             
                             # Send as audio
                             with open(audio_path, 'rb') as f:
@@ -1726,7 +1726,7 @@ def handle_yt_audio_callback(call):
                         mp3_files = list(Path(tmpdir).glob('*.mp3'))
                         if mp3_files:
                             mp3_path = mp3_files[0]
-                            caption = f"<b>🎵 YouTube Audio (MP3)</b>\n<b>Title:</b> {title}\n\n<i>Downloaded by @TeraInstaShortsDownloaderbot</i>"
+                            caption = f"<b>🎵 YouTube Audio (MP3)</b>\n<b>Title:</b> {title}\n\n<i>Downloaded by 💜 @TeraInstaShortsDownloaderbot</i>"
                             
                             # Send as audio
                             with open(mp3_path, 'rb') as f:
@@ -1784,7 +1784,7 @@ def handle_yt_audio_callback(call):
                             audio_files = list(Path(tmpdir).glob('*.*'))
                             if audio_files:
                                 audio_path = audio_files[0]
-                                caption = f"<b>🎵 YouTube Audio</b>\n<b>Title:</b> {title}\n\n<i>Downloaded by @TeraInstaShortsDownloaderbot</i>"
+                                caption = f"<b>🎵 YouTube Audio</b>\n<b>Title:</b> {title}\n\n<i>Downloaded by 💜 @TeraInstaShortsDownloaderbot</i>"
                                 
                                 with open(audio_path, 'rb') as f:
                                     bot.send_audio(
